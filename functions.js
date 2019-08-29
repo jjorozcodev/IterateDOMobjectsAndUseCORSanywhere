f48dc26f0e48e054fb4729f73b722708728899e7
JJ <jj.orozcolopez.13@gmail.com>
2019-08-29 11:51:06 -0600

    Consuming ApiRest using CORS and AJAX.
    Using cors-anywhere to consuming ApiRest method to get data list and show into a container.

---
+++ functions.js
@@ -0,0 +1,29 @@
+function GetDataList() {
+    var myUrl = 'http://yosoyjj-001-site1.btempurl.com/api/Estudiantes';
+    var proxy = 'https://cors-anywhere.herokuapp.com/';
+
+    var finalURL = proxy + myUrl;
+    // GET usando AJAX y JQuery
+    $.get(finalURL).done(function (resp) {
+
+        $.each(resp, function () {
+
+            $("#dataCards").append(
+                '<div class="col-md-3 mt-3">'
+                + '<div class="card">'
+                + '<div class="card-body">'
+                + '<h5 class="card-title">' + this.NombreCompleto + '</h5>'
+                + '<h6 class="card-subtitle mb-2 text-muted">ID: ' + this.NumCarnet + '</h6>'
+                + '<label class="btn btn-default">'
+                + 'Selected?:&emsp;'
+                + '<input data-idE="' + this.Id + '" type="checkbox" autocomplete="off">'
+                + '</label>'
+                + '</div>'
+                + '</div>'
+                + '</div>'
+            );
+
+        });
+
+    }).fail();
+}
